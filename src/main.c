fe45bebf3029d65859f13a2c2da27d8c3b70104e
Josephine Emenya <115475933+josieemenya@users.noreply.github.com>
2025-06-10 22:28:24 +0100

    LIKE

--- src/main.c
+++ src/main.c
@@ -4,10 +4,16 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include "../assets/tsetBones.c"
 
 
 
 int main(void){
-    printf("boobies");
+    
+    set_sprite_data(0, 16, tsetBones);
+    set_sprite_tile(2, 3);
+    move_sprite(0, TSETBONES_HEIGHT/2, TSETBONES_WIDTH/2);
+    SHOW_SPRITES;
+    
     return 0;
 }
