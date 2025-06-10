6d986989dfa6b8d0d36a78e706ea1721db007ba9
Josephine Emenya <115475933+josieemenya@users.noreply.github.com>
2025-06-10 22:57:31 +0100

    FRIED CHICKIEN (gnarly)

--- src/main.c
+++ src/main.c
@@ -9,11 +9,16 @@
 
 
 int main(void){
-    
+
     set_sprite_data(0, 16, tsetBones);
-    set_sprite_tile(2, 3);
-    move_sprite(0, TSETBONES_HEIGHT/2, TSETBONES_WIDTH/2);
+    set_sprite_tile(0, 3); // use tile 3
+    move_sprite(0, 80, 72); // visible position
+
     SHOW_SPRITES;
-    
+    DISPLAY_ON;
+
+    while(1) {
+        wait_vbl_done();
+    }
     return 0;
 }
