8bbbf587d3c510b0a444ad3a8b21e04246805f69
Josephine Emenya <115475933+josieemenya@users.noreply.github.com>
2025-06-30 02:01:40 +0100

    updated game_mode
    i think i was tryign to go for more intetional/SOLID programming

---
+++ includes/game_mode.h
@@ -0,0 +1,14 @@
+#include <gb/gb.h>
+#include <gb/sgb.h>
+#include <gbdk/console.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
+typedef enum {
+
+    start_game = 1,
+    menu_game = 2, 
+
+
+} GameMode;
